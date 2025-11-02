1d65016252f4da8447439e20e83f51df08fa0bed
Ken Suzurimi <suzurimi.ken@gmail.com>
2025-11-02 02:15:12 -0500

    Implement piece placement and flipping in 8 directions

--- Assets/Script/ReversiScript.cs
+++ Assets/Script/ReversiScript.cs
@@ -1,7 +1,10 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using System.Diagnostics.Tracing;
+using System.Threading;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Threading;
 
 public class ReversiScript : MonoBehaviour
 {
@@ -21,6 +24,7 @@ public class ReversiScript : MonoBehaviour
 
     int cube_gridX = 0, cube_gridY = 0;
 
+    private List<(int, int)> _InfoList = new List<(int, int)>();
 
     public enum spriteState
     {
@@ -40,7 +44,7 @@ public class ReversiScript : MonoBehaviour
     {
         for (int x = 0; x < FIELD_SIZE_X; x++)
         {
-            for (int y = 0; y < FIELD_SIZE_X; y++)
+            for (int y = 0; y < FIELD_SIZE_Y; y++)
             {
                 var sprite = Instantiate(ReversiSplite, new Vector3(1.01f * x, 0, 1.01f * y), Quaternion.Euler(90, 0, 0));
 
@@ -63,6 +67,7 @@ public class ReversiScript : MonoBehaviour
     {
         var k = Keyboard.current;
 
+        #region setting coordinates
         if (k.rightArrowKey.wasPressedThisFrame && cube_gridX < CUBE_MAX_X)
             cube_gridX++;
         else if (k.leftArrowKey.wasPressedThisFrame && cube_gridX > CUBE_MIN_X)
@@ -71,17 +76,39 @@ public class ReversiScript : MonoBehaviour
             cube_gridY++;
         else if (k.downArrowKey.wasPressedThisFrame && cube_gridY > CUBE_MIN_Y)
             cube_gridY--;
+        #endregion
 
         ApplyCubePosition();
 
+        var turncheck = false;
         if (k.enterKey.wasPressedThisFrame)
         {
-            _FieldState[cube_gridX, cube_gridY] = _PlayerTurn;
-            _PlayerTurn = _PlayerTurn == spriteState.Black ? spriteState.White : spriteState.Black;
+            for (int i = 0; i <= 7; i++)
+            {
+                if (TurnCheck(i))
+                {
+                    turncheck = true;
+                }
+            }
+
+            if (turncheck)
+            {
+                foreach (var info in _InfoList)
+                {
+                    var cube_gridX = info.Item1;
+                    var cube_gridY = info.Item2;
+                    _FieldState[cube_gridX, cube_gridY] = _PlayerTurn;
+                }
+
+                _FieldState[cube_gridX, cube_gridY] = _PlayerTurn;
+                _PlayerTurn = _PlayerTurn == spriteState.Black ? spriteState.White : spriteState.Black;
+                _InfoList.Clear();
+            }
+
         }
         for (int x = 0; x < FIELD_SIZE_X; x++)
         {
-            for (int y = 0; y < FIELD_SIZE_X; y++)
+            for (int y = 0; y < FIELD_SIZE_Y; y++)
             {
                 _FieldSpriteState[x, y].SetState(_FieldState[x, y]);
             }
@@ -91,5 +118,68 @@ public class ReversiScript : MonoBehaviour
     void ApplyCubePosition()
     {
         Cube.transform.localPosition = new Vector3(cube_gridX * CUBE_STEP, CUBE_FIXED_Y, cube_gridY * CUBE_STEP);
+    }
+
+    private bool TurnCheck(int direction)
+    {
+        var turnCheck = false;
+
+        var x = cube_gridX;
+        var y = cube_gridY;
+
+        var OpponentPlayerTurn = _PlayerTurn == spriteState.Black ? spriteState.White : spriteState.Black;
+        var infoList = new List<(int, int)>();
+
+        // Direction vectors
+        (int dx, int dy) = direction switch
+        {
+            0 => (-1, 0), // Left
+            1 => (1, 0), // Right
+            2 => (0, -1), // Down
+            3 => (0, 1), // Up
+            4 => (1, 1), // Upper Right
+            5 => (-1, -1), // Lower Left
+            6 => (-1, 1), // Upper Right
+            7 => (1, -1), // Lower Right
+            _ => (0, 0),
+        };
+
+        // Check by moving along each direction
+        while (true)
+        {
+            // Check if next position is outside the board before moving
+            int nx = x + dx;
+            int ny = y + dy;
+            if (nx < 0 || nx >= FIELD_SIZE_X || ny < 0 || ny >= FIELD_SIZE_Y)
+                break;
+
+            x = nx; y = ny;
+            var cell = _FieldState[x, y];
+
+            // Opponent piece -> store as flip candidate
+            if (cell == OpponentPlayerTurn)
+            {
+                infoList.Add((x, y));
+                continue;
+            }
+
+            // If no flipped pieces yet and reached own piece or empty -> cannot place
+            if (infoList.Count == 0 && (cell == _PlayerTurn || cell == spriteState.None))
+                break;
+
+            // Reached own piece -> success if opponent pieces were sandwiched
+            if (cell == _PlayerTurn)
+            {
+                turnCheck = infoList.Count > 0;
+                if (turnCheck)
+                    _InfoList.AddRange(infoList); //Add to flip list
+                break;
+            }
+
+            // Reached empty cell -> cannot place
+            if (cell == spriteState.None)
+                break;
+        }
+        return turnCheck;
     }
 }
